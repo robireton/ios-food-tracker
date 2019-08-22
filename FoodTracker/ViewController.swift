f63e86ed6b357f469009f6181b1b60613a235338
Rob Ireton <rob.ireton@uc.edu>
2019-08-22 10:44:57 -0400

    Initial Commit

---
+++ FoodTracker/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  FoodTracker
+//
+//  Created by Rob Ireton on 8/22/19.
+//  Copyright © 2019 University of Cincinnati. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
